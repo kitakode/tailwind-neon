7901d7cb6bedc4c6d110f3bc459ed5020cad2172
Mohammad Jamil Asfihani <mohammadjamilasfihani@gmail.com>
2023-05-31 17:04:42 +0700

    [v1.x]: add more colors

--- src/colors/index.js
+++ src/colors/index.js
@@ -3,7 +3,9 @@ const colors = require("tailwindcss/colors");
 
 // module - colors
 let _colors = {
-    'neon-blue': colors.cyan[500],
+    'neon-blue': colors.blue[500],
+    'neon-pink': colors.pink[500],
+    'neon-cyan': colors.cyan[500],
 };
 
 /** @type {import('tailwindcss').Config} */
