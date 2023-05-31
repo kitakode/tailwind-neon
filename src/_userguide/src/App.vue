eac61904b3038d798e435f820f72fbf073fddcd0
Mohammad Jamil Asfihani <mohammadjamilasfihani@gmail.com>
2023-05-31 17:55:11 +0700

    Revert "[v1.x]: init the user guide"
    This reverts commit d6a33a86bed97741e6bc253f6c07d47d29a78f89.

--- src/_userguide/src/App.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <p class="text-neon-blue">The quick brown fox...</p>
-    <p class="text-neon-pink">The quick brown fox...</p>
-    <p class="text-neon-cyan">The quick brown fox...</p>
-</template>
