8ff8ff70a4c4ca601cfd366d1a6465b190494e0d
Mohammad Jamil Asfihani <mohammadjamilasfihani@gmail.com>
2023-06-01 18:26:21 +0700

    [v1.x]: update App.vue

--- src/_userguide/src/App.vue
+++ src/_userguide/src/App.vue
@@ -1,5 +1,24 @@
 <template>
-    <p class="text-neon-blue">The quick brown fox...</p>
-    <p class="text-neon-pink">The quick brown fox...</p>
-    <p class="text-neon-cyan">The quick brown fox...</p>
+    <p class="text-neon-slate">Lorem ipsum dolor sit...</p>
+    <p class="text-neon-gray">Lorem ipsum dolor sit...</p>
+    <p class="text-neon-zinc">Lorem ipsum dolor sit...</p>
+    <p class="text-neon-neutral">Lorem ipsum dolor sit...</p>
+    <p class="text-neon-stone">Lorem ipsum dolor sit...</p>
+    <p class="text-neon-red">Lorem ipsum dolor sit...</p>
+    <p class="text-neon-orange">Lorem ipsum dolor sit...</p>
+    <p class="text-neon-amber">Lorem ipsum dolor sit...</p>
+    <p class="text-neon-yellow">Lorem ipsum dolor sit...</p>
+    <p class="text-neon-lime">Lorem ipsum dolor sit...</p>
+    <p class="text-neon-green">Lorem ipsum dolor sit...</p>
+    <p class="text-neon-emerald">Lorem ipsum dolor sit...</p>
+    <p class="text-neon-teal">Lorem ipsum dolor sit...</p>
+    <p class="text-neon-cyan">Lorem ipsum dolor sit...</p>
+    <p class="text-neon-sky">Lorem ipsum dolor sit...</p>
+    <p class="text-neon-blue">Lorem ipsum dolor sit...</p>
+    <p class="text-neon-indigo">Lorem ipsum dolor sit...</p>
+    <p class="text-neon-violet">Lorem ipsum dolor sit...</p>
+    <p class="text-neon-purple">Lorem ipsum dolor sit...</p>
+    <p class="text-neon-fuchsia">Lorem ipsum dolor sit...</p>
+    <p class="text-neon-pink">Lorem ipsum dolor sit...</p>
+    <p class="text-neon-rose">Lorem ipsum dolor sit...</p>
 </template>
